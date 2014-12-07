df44609599cce6d30ea88f305be71a4202990bb9
John Foerch <jjfoerch@earthlink.net>
2014-12-07 18:36:18 -0500

    license with LGPL 3, same license that OLA uses.

--- ola-impl.scm
+++ ola-impl.scm
@@ -1,4 +1,21 @@
 
+;; This file is part of ola-egg.
+;; Copyright (C) 2014  John J. Foerch
+;;
+;; ola-egg is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU Lesser General Public License as published
+;; by the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; ola-egg is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+;; License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with ola-egg.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
 (foreign-declare "#include <ola/DmxBuffer.h>")
 (foreign-declare "#include <ola/Logging.h>")
 (foreign-declare "#include <ola/client/StreamingClient.h>")
